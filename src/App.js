9b6703fc5fbae20b16c71c2defa55c8f74aa001f
Oscar <ofuentese@gmail.com>
2019-07-03 13:33:54 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 /* Componentes */
 import Navegacion from './componentes/nav'
 import Captura from './componentes/captura'
+import Tarjetas from './componentes/tarjetas'
 
-function App() {
-  return (
-    <div className="App">
-      <Navegacion/>
+/* datos dummie */
+import {ejemplos} from './ejemplos.json';
+
+
+export default class App extends Component {
+  constructor(){
+    /* se ejecuta antes que los otros métodos */
+    super(); /* con esto heredamos los procedimientos de la clase extends, y no marque error */
+    this.state = {ejemplos}
+  }
+
+  /* Funciones Generales de la App */
+  borrarTarjeta(i){
+  
+  }
+  
+  render(){
+    const contactos = this.state.ejemplos.map((tarjeta, i) => {
+      return ( <Tarjetas tarjeta={tarjeta} key= {i} index={i} borrarTarjeta={this.borrarTarjeta.bind(this)}/>);
+    });
+    return (
+      <div className="App">
+        <Navegacion/>
       <div className="row">
         <div className="col-md-3 border-right">
           <Captura />
         </div>
         <div className="col-md-9">
           <div className="row">
-
+            {contactos}
           </div>
         </div>
       </div>
     </div>
-  );
+    );
+  }
+/* fin class */
 }
 
-export default App;
+
---
+++ src/componentes/tarjetas.js
@@ -0,0 +1,23 @@
+/* Componente de las tarjetas de los datos de contacto */
+import React, { Component } from "react";
+
+export default class Tarjetas extends Component {
+    render(){
+        return(
+            <div className="col-md-3">
+                <div className="card mt-4">
+                    <div className="card-header">
+                        <h3>{this.props.tarjeta.nombre}</h3>
+                    </div>
+                    <div className="card-body">
+                        <p>{this.props.tarjeta.tel}</p>
+                        <p>{this.props.tarjeta.correo}</p>
+                    </div>
+                    <div className="card-footer">
+                        <button className="btn btn-danger" onClick={this.props.borrarTarjeta.bind(this, this.props.index)}>Borrar</button>
+                    </div> 
+                </div>
+            </div>
+        );
+    }
+}
